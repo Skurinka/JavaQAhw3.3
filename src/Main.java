29c059729f040c80c1fa39ace15f5fdde3684b08
Skurinka <nikitametmen@gmail.com>
2022-07-06 17:51:47 +0300

    formula fixed

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         double S = 1_000_000; //сумма кредита
         double n = 36; //период
-        double i = 0.0999 / n; //процентная ставка
+        double i = 0.099 /12; //процентная ставка
         double K = service.calculate(i, n);
         double A = (int) (K * S);
         System.out.print("Сумма ежемесячного платежа: " + A);
